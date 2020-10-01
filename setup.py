5280a9cb7def362eb670f5884892da1eca962c6c
JorgeDeLosSantos <delossantosmfq@gmail.com>
2020-09-30 21:23:03 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+import os
+
+dir_setup = os.path.dirname(os.path.realpath(__file__))
+
+with open(os.path.join(dir_setup, 'moro', 'version.py')) as f:
+    # Defines __version__
+    exec(f.read())
+
+setup(name='moro',
+      version=__version__,
+      description='Python library for modeling of robots',
+      author='Pedro Jorge De Los Santos',
+      author_email='delossantosmfq@gmail.com',
+      license = "MIT",
+      keywords=["Robotics","Kinematics","Dynamics"],
+      url='https://github.com/moro',
+      packages=['moro',],
+      install_requires=['sympy','matplotlib'],
+      classifiers=[
+      "Development Status :: 2 - Pre-Alpha",
+      "Intended Audience :: Education",
+      "Intended Audience :: Developers",
+      "License :: OSI Approved :: MIT License",
+      "Operating System :: OS Independent",
+      "Programming Language :: Python",
+      "Programming Language :: Python :: 3.6",
+      "Programming Language :: Python :: Implementation :: CPython",
+      ]
+      )
